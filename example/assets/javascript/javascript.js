bc4a7295b88d5975df355b8486d06b7651ea95e1
Anish <anish.kottu@duke.edu>
2020-05-08 16:01:18 -0400

    centered footer and added example template of website

---
+++ example/assets/javascript/javascript.js
@@ -0,0 +1,25 @@
+// header button page redirects
+
+$('#home-header-button').click(function(){
+   window.location.href='index.html';
+})
+
+$('#episodes-header-button').click(function(){
+   window.location.href='episodes.html';
+})
+
+$('#about-header-button').click(function(){
+   window.location.href='about.html';
+})
+
+$('#staff-header-button').click(function(){
+   window.location.href='staff.html';
+})
+
+$('#guests-header-button').click(function(){
+   window.location.href='guests.html';
+})
+
+$('#contact-header-button').click(function(){
+   window.location.href='contact.html';
+})
